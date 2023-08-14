ab11d4095a7b577f11910ea15ea50f4649107d2d
Bezluydniy Anton <abezlyudniy@ozon.ru>
2023-08-14 12:09:08 +0300

    feat: wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require("wezterm")
 return {
-	color_scheme = "Dracula (Official)",
-	font = wezterm.font("VictorMono Nerd Font Mono"),
-	font_size = 20,
+    color_scheme = "Dracula (Official)",
+    font = wezterm.font("Victor Mono"),
+    font_size = 20,
+    enable_tab_bar = false,
+    window_padding = {
+        bottom = 0,
+    },
 }
