6a86503e884b3edcc8873ddb0db73901f21cf3e6
abezlyudniy <abezlyudniy@ozon.ru>
2023-08-22 15:16:49 +0300

    feat: wezterm fonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 return {
-    color_scheme = "Dracula (Official)",
-    font = wezterm.font("Victor Mono"),
-    font_size = 20,
-    enable_tab_bar = false,
-    window_padding = {
-        bottom = 0,
-    },
+	color_scheme = "Dracula (Official)",
+	font = wezterm.font("Victor Mono"),
+	font_size = 14,
+	enable_tab_bar = false,
+	window_padding = {
+		bottom = 0,
+	},
 }
