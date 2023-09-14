c54529ea5842701d326f7147704ee421f142ffc9
abezlyudniy <abezlyudniy@ozon.ru>
2023-09-14 16:46:41 +0700

    feat: wezterm removed borders

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,12 @@ return {
 	color_scheme = "Dracula (Official)",
 	font = wezterm.font("Victor Mono"),
 	font_size = 14,
+	window_decorations = 'RESIZE',
 	enable_tab_bar = false,
 	window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
 		bottom = 0,
 	},
 	send_composed_key_when_left_alt_is_pressed = true,
