238d3481e25e80a8f2e6b86f7fd015e8048ee9a1
bezlant <abezlyudniy@Snowcrash.local>
2025-12-31 12:55:28 +0800

    feat: snacks picker c+f

--- fish/functions/personal/fish_user_key_bindings.fish
+++ fish/functions/personal/fish_user_key_bindings.fish
@@ -3,4 +3,8 @@ function fish_user_key_bindings
 
     # Bind "p" in normal mode to paste from system clipboard
     bind -M default p 'commandline -i (pbpaste)'
+
+    # Bind Ctrl+F to open nvim with Snacks picker files
+    bind -M insert \cf 'commandline -r ""; nvim -c "lua Snacks.picker.files()"'
+    bind -M default \cf 'commandline -r ""; nvim -c "lua Snacks.picker.files()"'
 end
