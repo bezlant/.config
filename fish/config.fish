0e7259ed7858acb62d36a1318a6fee8970d9a730
bezlant <89563512+bezlant@users.noreply.github.com>
2026-04-02 13:33:45 +0700

    feat: print

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,15 @@ pyenv init - | source
 
 alias notes='nvim "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes/"'
 alias cdo='cd "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes/"'
+
+# HP Smart Tank 580 printing
+function print
+    if test (count $argv) -eq 0
+        echo "Usage: print <file>"
+        return 1
+    end
+    # Wake CUPS if needed
+    curl -s http://localhost:631/ >/dev/null 2>&1
+    sleep 1
+    lp -d HP_Smart_Tank_580 $argv
+end
